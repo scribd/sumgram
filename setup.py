ca8d15f01a27cc83f3d20b01daddb653076f1e04
Antonia Mouawad <antoniam@scribd.com>
2020-04-30 11:53:09 -0400

    Add data and change setup accordingly

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from sumgram import __version__
 
 desc = """sumgram is a tool that summarizes a collection of text documents by generating the most frequent sumgrams (conjoined ngrams)"""
 
-
 setup(
     name='sumgram',
     version=__version__,
@@ -15,16 +14,17 @@ setup(
     author_email='anwala@cs.odu.edu',
     url='https://github.com/oduwsdl/sumgram/',
     packages=find_packages(),
+    package_data={
+        'sumgram': ['data/cleaning/*', 'data/stopwords_lists/*']
+    },
     license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ],
     install_requires=[
-        'numpy==1.17.0',
-        'requests==2.22.0',
-        'sklearn==0.0'
-    ],
-    entry_points={'console_scripts': ['sumgram = sumgram.sumgram:main']}
+        'numpy',
+        'sklearn',
+        'regex',
+        'langdetect',
+        'nltk',
+        'matplotlib',
+        'pandas'
+    ]
 )
